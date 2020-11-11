05d6bdc4ca316ab455b6e15c5b084226d4b3a976
swang1112 <wangshu1112@gmail.com>
2020-11-11 13:59:06 +0100

    Update E01.m

--- E01.m
+++ E01.m
@@ -15,6 +15,22 @@ D = A(:,1:3);
 F = A(:,[2,4]);
 
 %% 3.
-transpose(A);
-inv(A);
-det(A)
+B'
+inv(B)
+det(B)
+
+%% 4.
+z = randn(100,1);
+z_mean = sum(z) / 100;
+mean(z) - z_mean
+square_div = z-z_mean;
+z_var = square_div' * square_div /99;
+z_std = sqrt(z_var);
+z_var - var(z)
+z_std - std(z)
+
+%% 5.
+clc
+for i=1:100
+    if
+end
