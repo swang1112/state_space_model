a4bc4d0a552adf2b12f6e61d072c44e2c48e9089
swang1112 <wangshu1112@gmail.com>
2020-11-13 18:16:23 +0100

    Update df_test.m

--- df_test.m
+++ df_test.m
@@ -6,12 +6,7 @@ DF_stat = zeros(R,1);
 ar_coeff = zeros(R,1);
     for r=1:R
         [alpha_hat, alpha_se] = OLS(y(1:(T-1),r), y(2:T,r));
-        
         ar_coeff(r) = alpha_hat;
-        %ar_coeff(r) = inv(y(1:(T-1),r)'*y(1:(T-1),r)) * y(1:(T-1),r)'*y(2:T,r);
-        %SSR = y(2:T,r)'*y(2:T,r) - ar_coeff(r) * y(1:(T-1),r)'*y(2:T,r);
-        %sigma2 = SSR/ (T-1);
-        %alpha_se = sqrt(sigma2) / (y(1:(T-1),r)'*y(1:(T-1),r));
         DF_stat(r) = (ar_coeff(r) - 1) / alpha_se;
     end
 end
