3c525974cff24f647ff90c35fa59cc73455f04b1
swang1112 <wangshu1112@gmail.com>
2020-12-09 21:16:48 +0100

    MNZ

---
+++ Kalman_basic.m
@@ -0,0 +1,79 @@
+function [alpha, P, pred_error, pred_error_cov] = Kalman_basic(y, alpha_0, P_0, A, X, H, F, Mu, Sigma_obs, Sigma_trans, Smooth)
+%   y:              Txn matrix, data matrix of obs
+%   alpha_0:        rx1 vector, init state 
+%   P_0:            rxr matrix, init state conditional covar matrix
+%   A:              nxk matrix, coeff matrix for exo obs
+%   X:              Txk matrix, data matrix of exo obs
+%   H:              nxr matrix, state loading
+%   F:              rxr matrix, AR coeff matrix in the trans equation
+%   Mu:             rx1 matrix, intercept in the trans equation
+%   Sigma_obs:      nxn matrix, error covar matrix in the obs equation
+%   Sigma_trans:    rxr matrix, error covar matrix in the trans equation
+%   Smooth:         bool, if Smooth=true, smoothed estimates of states by backwards
+%                   iterationss are returned
+
+    [Tob, n]    = size(y);
+    k           = size(X, 2);
+    r           = size(H, 2);
+
+    % storage reservation
+    alpha           = zeros(Tob,r);             
+    P               = zeros(r,r,Tob);              
+    pred_error      = zeros(Tob,n);               % prediction error
+    pred_error_cov  = repmat(eye(n), [1,1,Tob]);  % prediction error covariance
+    
+    if Smooth == 1
+        P_10s           = repmat(eye(r), [1,1,Tob]);
+        alpha_10s       = zeros(Tob,r);      
+        %P_10s(:,:,1)    = P_0;
+        %alpha_10s(1,:)  = alpha_0';
+    end
+
+    % init
+    alpha(1,:)  = alpha_0';
+    P(:,:,1)    = P_0;
+
+    for t = 2:Tob;
+        % prediction state
+        alpha_10 = Mu + F * alpha(t-1,:)';
+        P_10     = F * P(:,:,t-1) * F' + Sigma_trans;
+    
+        % prediction obs
+        v_10 = y(t,:)' - H * alpha_10 - A * X(t,:)';
+        f_10 = H * P_10 * H' + Sigma_obs;
+    
+        % update
+        K        = P_10 * H' * inv(f_10);
+        alpha_11 = alpha_10 + K * v_10;
+        P_11     = P_10 - K * H * P_10; 
+    
+        % store
+        pred_error(t,:)       = v_10';
+        pred_error_cov(:,:,t) = f_10;
+        alpha(t,:)            = alpha_11';
+        P(:,:,t)              = P_11;
+        
+        if Smooth == 1
+            P_10s(:,:,t)    = P_10;
+            alpha_10s(t,:)  = alpha_10';
+        end
+    
+    end
+
+    % smoothing
+    if Smooth == 1
+        % copy results from filtering
+        alpha_smo   = alpha;
+        P_smo       = P;
+        for t = Tob-1:-1:1
+            J               = P(:,:,t) * F' * inv(P_10s(:,:,t+1));
+            alpha_smo(t,:)  = alpha(t,:) +  (alpha_smo(t+1,:) - alpha_10s(t+1,:)) * J';
+            P_smo(:,:,t)    = P(:,:,t) +  J * (P_smo(:,:,t+1) - P_10s(:,:,t+1)) * J';
+        end
+        % replace filtering results
+        alpha   = alpha_smo;
+        P       = P_smo;
+    end
+    
+
+end
--- nile.m
+++ nile.m
@@ -11,6 +11,7 @@ Y = importdata("nile.txt");
 
 
 %%
+figure(1);
 subplot(2,2,1);
 plot(2:100, [Y(2:end), alpha(2:end)], 'LineWidth', 1.5);
 title("Data and alpha");
@@ -22,5 +23,39 @@ plot(3:100, v(3:end));
 title("prediction error: v");
 subplot(2,2,4);
 plot(3:100, F(3:end));
+title("prediction error variance: f");
+
+
+%%
+%addpath('MNZ');
+Y = importdata("nile.txt");
+Tob = length(Y);
+%% Known parameters
+A   = 0;
+X   = zeros(Tob, 1);
+H   = 1;
+F   = 1;
+Mu  = 0;
+Sigma_obs = 15099;
+Sigma_trans = 1469.1;
+%% init
+alpha_0 = 0;
+P_0 = 10^7;
+
+[alpha, P, v, f] = Kalman_basic(Y, alpha_0', P_0, A, X, H, F, Mu, Sigma_obs, Sigma_trans, 1);
+
+%%
+figure(2);
+subplot(2,2,1);
+plot(2:100, [Y(2:end), alpha(2:end)], 'LineWidth', 1.5);
+title("Data and alpha");
+subplot(2,2,2);
+plot(2:100, squeeze( P(2:end)));
+title("state variance: P");
+subplot(2,2,3);
+plot(3:100, v(3:end));
+title("prediction error: v");
+subplot(2,2,4);
+plot(3:100, squeeze(f(3:end)));
 title("prediction error variance: f");
 
